b7efcb5d657d48ce6ba657d94b9cfba7e82d2dd4
Paulo Mota <paulob1@hotmail.com>
2025-04-25 16:41:13 -0300

    Melhoria na consulta de tickets

--- frontend/src/components/TicketsManagerTabs/index.js
+++ frontend/src/components/TicketsManagerTabs/index.js
@@ -934,6 +934,7 @@ const TicketsManagerTabs = () => {
                     classes={{ badge: classes.customBadge }}
                     badgeContent={openCount}
                     color="primary"
+                    max={999}
                   >
                     <MessageSharpIcon
                       style={{
@@ -970,6 +971,7 @@ const TicketsManagerTabs = () => {
                     classes={{ badge: classes.customBadge }}
                     badgeContent={pendingCount}
                     color="primary"
+                    max={999}
                   >
                     <ClockIcon
                       style={{
@@ -1007,6 +1009,7 @@ const TicketsManagerTabs = () => {
                       classes={{ badge: classes.customBadge }}
                       badgeContent={groupingCount}
                       color="primary"
+                      max={999}
                     >
                       <Group
                         style={{
