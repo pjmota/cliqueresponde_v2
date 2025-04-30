40319686d2faab3351413d22f85726f1de49fb68
Paulo Mota <paulob1@hotmail.com>
2025-04-30 09:00:46 -0300

    Correção de informação, so mostrar em (Atendimentos não continuados) somente tickets com mensagens de clientes.

--- backend/src/services/ReportService/DashboardTicketsHappeningsNotConotinued.ts
+++ backend/src/services/ReportService/DashboardTicketsHappeningsNotConotinued.ts
@@ -43,8 +43,9 @@ export default async function getHappeningsNotContinued(
       where
         t.status = 'open'
         and t."companyId" = ${companyId}
-        --and t."fromMe" = 'false'
-        and t."isGroup" = 'false'
+        and t."fromMe" = false
+        and m."fromMe" = false
+        and t."isGroup" = false
         --and t."updatedAt" < '${date}'
       group by
         t.id,
@@ -61,7 +62,7 @@ export default async function getHappeningsNotContinued(
     query,
     { 
       type: QueryTypes.SELECT, 
-      //logging: console.log 
+      logging: console.log 
     }
   );
   
